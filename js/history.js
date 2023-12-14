9eab751c8fc5a14b608fa86ab6459a6eb8a76f54
CHANTHOU.VOEUN <CHANTHOU.VOEUN@PNC.LAN>
2023-12-14 09:20:25 +0700

    #18 fix code in product list

--- js/history.js
+++ js/history.js
@@ -1,11 +1,11 @@
-let header = document.querySelector('header');
-let container = document.querySelector('.container');
-let openTab = document.querySelector('.box3');
-let productTab = document.querySelector('.box1');
-let history = document.querySelector('.history');
+// let header = document.querySelector('header');
+// let container = document.querySelector('.container');
+// let openTab = document.querySelector('.box3');
+// let productTab = document.querySelector('.box1');
+// let history = document.querySelector('.history');
 
-openTab.addEventListener('click', ()=>{
-    container.style.display = 'none';
-    history.style.display = 'block';
-});
+// openTab.addEventListener('click', ()=>{
+//     container.style.display = 'none';
+//     history.style.display = 'block';
+// });
 
